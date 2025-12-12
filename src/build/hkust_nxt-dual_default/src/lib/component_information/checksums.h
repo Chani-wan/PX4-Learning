fbff47e320319da74a37883f053a2c97cfadce80
Chani-wan <chaniwan2003@gmail.com>
2025-12-12 11:42:47 +0800

    1

--- src/build/hkust_nxt-dual_default/src/lib/component_information/checksums.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-namespace component_information {
-static constexpr uint32_t component_general_crc = 3176172985;
-static constexpr const char *component_general_sha256 = "8fe0406dc34712c0f2ac8f2b28facd5d1acd7559f4fae3c6c8b677764beddd1b";
-static constexpr uint32_t all_events_crc = 3380907780;
-static constexpr const char *all_events_sha256 = "02c5f8e89959fe3217304584c2be2f33f6b576e2711a586d5ec825394b81dd8b";
-}
